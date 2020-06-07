bf6d7531a981d41486717c79c1e6ddd3fecac6a7
nzh693 <nzh_693@163.com>
2020-06-07 14:51:10 +0800

    test

--- src/main/java/com/bug/conroller/TestUserController.java
+++ src/main/java/com/bug/conroller/TestUserController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class UserController {
+public class TestUserController {
 
     @Autowired
     private UserServiceImp serviceImp;
@@ -25,6 +25,8 @@ public class UserController {
         int c=a/b;
         return "delete One";
     }
+
+
 
 
 }
